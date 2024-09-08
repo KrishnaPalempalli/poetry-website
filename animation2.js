d695a3adae299dcbe0ae0196229f1023f9f3e81e
KrishnaPalempalli <59264805+KrishnaPalempalli@users.noreply.github.com>
2024-09-08 11:26:44 -0400

    Uploaded All Poetry Website Files

---
+++ animation2.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+//  alert("Welcome Everyone!");
+//  alert("Over the next few minutes, you will be exploring poetry in multiple ways!");
+//  alert("First, you will watch an overview video about poetry!");
+//  alert("Second, you will read some of my own poems to get a feeling for real poetry!");
+//  alert("Finally, you will read some interesting facts about poetry!");
+
+
+  });
